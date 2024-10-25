211355409eab7516b4967f9dc0b1ecda9a12f910
eduardo-meira <dudumeira76@gmail.com>
2024-10-25 16:11:55 +0100

    envio setup projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Teste'
+import EstilosGlobais from './Styles'
+
+function App() {
+  return (
+    <>
+      <EstilosGlobais />
+      <Teste />
+    </>
+  )
+}
+
+export default App
